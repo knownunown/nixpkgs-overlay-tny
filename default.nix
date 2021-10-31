3a1bf76f7ba36f33984a6691c21530c45957e85c
Andrew Pan <known@unown.me>
2021-10-31 10:54:34 -0500

    pkgs/overrides: discord@0.0.16

--- default.nix
+++ default.nix
@@ -2,4 +2,6 @@
   tbb = pkgs.callPackage ./pkgs/tbb { };
   libsimdpp = pkgs.callPackage ./pkgs/libsimdpp { };
   nupack = pkgs.callPackage ./pkgs/nupack { inherit tbb libsimdpp; };
+
+  discord = pkgs.callPackage ./pkgs/overrides/discord.nix { };
 }
---
+++ pkgs/overrides/discord.nix
@@ -0,0 +1,8 @@
+{ discord, fetchurl }:
+discord.overrideAttrs (_: rec {
+  version = "0.0.16";
+  src = fetchurl {
+    url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+    hash = "sha256-UTVKjs/i7C/m8141bXBsakQRFd/c//EmqqhKhkr1OOk=";
+  };
+})
