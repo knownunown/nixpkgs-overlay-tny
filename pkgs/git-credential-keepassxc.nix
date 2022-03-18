8b63def7bdd31b625dc7464c35709f9c89337d44
Andrew Pan <a@tny.town>
2022-03-18 15:27:47 -0500

    pkgs/git-credential-keepassxc: add required macOS DiskArbitration framework

--- pkgs/git-credential-keepassxc.nix
+++ pkgs/git-credential-keepassxc.nix
@@ -1,24 +1,29 @@
 { lib, fetchFromGitHub, rustPlatform, targetPlatform, darwin }:
 rustPlatform.buildRustPackage rec {
-  pname = "git-credential-keepassxc";
-  version = "0.8.1";
+    pname = "git-credential-keepassxc";
+    version = "0.8.1";
 
-  buildInputs = lib.optionals (targetPlatform.isDarwin) [ darwin.apple_sdk.frameworks.IOKit ];
-  src = fetchFromGitHub {
-    owner = "Frederick888";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-JhJu2/dcHBe1CAdMTMxlA0jA/wYQG3EBtf9POVe9SAQ=";
-  };
+    buildInputs = lib.optionals (prev.targetPlatform.isDarwin)
+      (with darwin.apple_sdk.frameworks; [
+        IOKit DiskArbitration Foundation
+      ]);
+    src = fetchFromGitHub {
+      owner = "Frederick888";
+      repo = pname;
+      rev = "v${version}";
+      #sha256 = "1kzq6mnffxfsh1q43c99aq2mgm60jp47cs389vg8qpd1cqh15nj0";
+      sha256 = "sha256-JhJu2/dcHBe1CAdMTMxlA0jA/wYQG3EBtf9POVe9SAQ=";
+    };
 
-  cargoSha256 = "sha256-62enbfDccfedPo0tICHPitDbZc+qu9t0Kh9kHeP/xzU=";
+    #cargoSha256 = "sha256-fglAwQB+UdOJeBcg9NForaxrel7ME9p8VP4k4Rj0jtU=";
+    cargoSha256 = "sha256-62enbfDccfedPo0tICHPitDbZc+qu9t0Kh9kHeP/xzU=";
 
-  meta = with lib; {
-    description = "Helper that allows Git (and shell scripts) to use KeePassXC as credential store";
-    homepage = "https://github.com/Frederick888/git-credential-keepassxc";
-    license = licenses.gpl3Only;
-    maintainers = [ "tny" ];
-  };
+    meta = with lib; {
+      description = "Helper that allows Git (and shell scripts) to use KeePassXC as credential store";
+      homepage = "https://github.com/Frederick888/git-credential-keepassxc";
+      license = licenses.gpl3Only;
+      maintainers = [ "tny" ];
+    };
 
-  doCheck = false;
-}
+    doCheck = false;
+};
