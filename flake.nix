5b2d232e5786330c937d0145154d19acc4fc497d
Andrew Pan <a@tny.town>
2022-03-18 15:01:25 -0500

    flake: nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-21.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
     nixpkgs-fmt.inputs.nixpkgs.follows = "nixpkgs";
