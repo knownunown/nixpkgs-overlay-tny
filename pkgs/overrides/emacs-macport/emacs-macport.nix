03deb1490dd98c93a7eb0939d4a9a0c136a6b159
Andrew Pan <a@tny.town>
2023-02-14 10:22:16 -0600

    emacsMacport: another crack at fixing the overrelease issue

--- pkgs/overrides/emacs-macport/emacs-macport.nix
+++ pkgs/overrides/emacs-macport/emacs-macport.nix
@@ -1,7 +1,7 @@
 { emacsMacport, llvmPackages_14, darwin }:
 (emacsMacport.overrideAttrs (o: {
   buildInputs = o.buildInputs ++ (with darwin.apple_sdk.frameworks; [ UniformTypeIdentifiers ]);
-  patches = o.patches ++ [ ./0001-mac-gui-loop-block-lifetime.patch ];
+  patches = o.patches ++ [ ./0002-mac-gui-loop-block-autorelease.patch ]; 
 })).override {
   llvmPackages_6 = llvmPackages_14;
 }
