23333ddb2ff3c2424ef9cc5bbeabee12b87ba4f2
Andrew Pan <a@tny.town>
2023-08-29 01:53:01 -0500

    emacsMacport: fix root cause of overrelease issue
    Signed-off-by: Andrew Pan <a@tny.town>

--- pkgs/overrides/emacs-macport/emacs-macport.nix
+++ pkgs/overrides/emacs-macport/emacs-macport.nix
@@ -1,7 +1,20 @@
-{ emacsMacport, llvmPackages_14, darwin }:
+{ lib, fetchFromBitbucket, emacsMacport, llvmPackages_14, darwin }:
+let
+  version = "29.1-mac-10.0";
+in
 (emacsMacport.overrideAttrs (o: {
-  buildInputs = o.buildInputs ++ (with darwin.apple_sdk.frameworks; [ UniformTypeIdentifiers ]);
-  patches = o.patches ++ [ ./0002-mac-gui-loop-block-autorelease.patch ]; 
+  inherit version;
+  name = "emacs-mac-${version}";
+
+  src = fetchFromBitbucket {
+    owner = "mituharu";
+    repo = "emacs-mac";
+    rev = "emacs-${version}";
+    hash = "sha256-TE829qJdPjeOQ+kD0SfyO8d5YpJjBge/g+nScwj+XVU=";
+  };
+
+  buildInputs = o.buildInputs ++ (with darwin.apple_sdk.frameworks; [ UniformTypeIdentifiers Accelerate ]);
+  CFLAGS = [ "-include ${./noescape_noop.h}" ];
 })).override {
   llvmPackages_6 = llvmPackages_14;
 }
