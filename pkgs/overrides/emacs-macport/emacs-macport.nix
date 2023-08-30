f1454f9af5772386844ef848ec296cf05091e5ee
Andrew Pan <a@tny.town>
2023-08-30 00:35:23 -0500

    emacs29-macport: `CFLAGS` -> `configureFlags`
    Signed-off-by: Andrew Pan <a@tny.town>

--- pkgs/overrides/emacs-macport/emacs-macport.nix
+++ pkgs/overrides/emacs-macport/emacs-macport.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchFromBitbucket, emacs29-macport, llvmPackages_14, darwin }:
 (emacs29-macport.overrideAttrs (o: {
   buildInputs = o.buildInputs ++ (with darwin.apple_sdk.frameworks; [ UniformTypeIdentifiers Accelerate ]);
-  CFLAGS = [ "-include ${./noescape_noop.h}" ];
+  configureFlags = o.configureFlags ++ [ "CFLAGS=-include${./noescape_noop.h}" ];
 })).override {
   llvmPackages_6 = llvmPackages_14;
 }
